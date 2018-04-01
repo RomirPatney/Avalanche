e645f152763d6355cf559cdc7c84317fd0d0d919
RomirPatney <romir.patney@gmail.com>
2018-03-31 21:47:50 -0400

    tables and data structs created

---
+++ Avalanche/Avalanche/Data/Migrations/20180401004139_cleaner.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Avalanche.Data.Migrations
+{
+    public partial class cleaner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    ID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    HeartBeat = table.Column<double>(nullable: false),
+                    Humidity = table.Column<double>(nullable: false),
+                    IsEmergency = table.Column<bool>(nullable: false),
+                    Temperature = table.Column<double>(nullable: false),
+                    Tremble = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
---
+++ Avalanche/Avalanche/Models/Tables/Log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Avalanche.Models.Tables
+{
+    public class Log
+    {
+        public DateTime Date { get; set; }
+        public double HeartBeat { get; set; }
+        public double Tremble { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public bool IsEmergency { get; set; }
+
+        [Key]
+        public Int64 ID { get; set; }
+
+        public Log(DateTime date, double heartBeat, double tremble, double temperature, double humidity, bool isEmergency, long iD)
+        {
+            Date = date;
+            HeartBeat = heartBeat;
+            Tremble = tremble;
+            Temperature = temperature;
+            Humidity = humidity;
+            IsEmergency = isEmergency;
+            ID = iD;
+        }
+
+        public Log()
+        {
+
+        }
+    }
+}
