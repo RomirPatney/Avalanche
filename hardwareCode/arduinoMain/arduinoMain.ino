d325e415fbffde49b4fd8072f0e0843b8b7cf2a1
Karan Bajaj <karan3663@gmail.com>
2018-03-31 18:39:46 -0700

    Got the temp sensor working (after godly debugging)

--- hardwareCode/arduinoMain/arduinoMain.ino
+++ hardwareCode/arduinoMain/arduinoMain.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+int sensePin = A0;  
+int sensorInput;    
+double temp;        
 
+void setup() {
+  Serial.begin(9600); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  
+  sensorInput = analogRead(A0);    
+  temp = (double)sensorInput / 1024;   
+  temp = temp * 5;                 
+  temp = temp - 0.5;               
+  temp = temp * 100;               
+
+  Serial.print("Current Temperature: ");
+  Serial.println(sensorInput);
+  Serial.println(temp);
+  Serial.println("End");
+
+  delay(200);
 }
